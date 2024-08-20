8fd47bfd6bfd7283b7916c71bb345f4736013a44
rpostek <robert@postek.eu>
2024-08-20 21:34:05 +0200

    remove PersonDetail and use AJAX for detailed view in tree view

--- sysusers/staff/urls.py
+++ sysusers/staff/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('department/<str:dep>/', views.DepartmentView.as_view(), name='department-staff'),
     path('room/<str:room>/', views.RoomView.as_view(), name='room-staff'),
     path('title/<str:title>/', views.TitleView.as_view(), name='title-staff'),
-    path('person/<int:id>/', views.PersonDetailView.as_view(), name='person-detail-staff'),
+    #path('person/<int:id>/', views.PersonDetailView.as_view(), name='person-detail-staff'),
     path('person/<int:id>/table/', views.PersonDetailViewTable.as_view(), name='person-detail-staff-table'),
-    path('tree/', views.TreeView.as_view(), name='tree'),
+    #path('tree/', views.TreeView.as_view(), name='tree'),
+    path('tree/<int:id>/', views.PersonTreeView.as_view(), name='person-tree'),
 ]
